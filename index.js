8d440c654f1da66603c60bcf8eb21d17622f5cd2
john <jmlovitsch@gmail.com>
2020-11-03 19:37:54 -0600

    commit tree 707df1aa1e44b68c9b28fa856701d23ef67c5c1e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const selectDifferentDrivers = (drivers, whichDrivers) => whichDrivers(drivers)
+
+const returnFirstTwoDrivers = (drivers)  => drivers.slice(0, 2)
+
+const returnLastTwoDrivers = (drivers)  => drivers.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => (value) => value * num
+
+
+const fareDoubler = (createFareMultiplier) => createFareMultiplier * 2
+
+
+const fareTripler = (createFareMultiplier) => createFareMultiplier * 3
+
+const randomDrivers = selectingDrivers.sample(1)
